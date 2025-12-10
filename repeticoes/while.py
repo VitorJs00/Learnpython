f787c6797d9a81a562a7884c7d6700eb9eb44778
VitorJs00 <161177573+VitorJs00@users.noreply.github.com>
2025-12-10 00:13:34 -0300

    while loop

--- repeticoes/while.py
+++ repeticoes/while.py
@@ -1,3 +1,4 @@
+"""
 nome= input('digite algom')
 while not nome:
     nome= input('digite algom')
@@ -5,8 +6,81 @@ print(f'o que digitou {nome}')
 
 
 i=0
-while i<=10000000:
-    print(i)
+while i<=40:
+     
+    if i%2 ==0:  
+        continue
+    else:
+        print(i)
+
+linhas = 5
+colunas =5
+linha =0
+while linha <=linhas:
+    coluna=0
+    while coluna<=colunas:
+        print((f'{linha=}'),(f'{coluna}coluna'))
+        coluna+=1
+    linha+=1
+    
+
+
+nome = "OLOKO"
+nova_string = ''
+i=0
+while i < len(nome):
+    nova_string+= f'*{nome[i]}'
     i+=1
-    if i ==100:
-        break
+print(nova_string)
+
+rodando = True
+
+while rodando:
+    try:
+        while rodando:
+            n1=int(input('numero 1'))
+            operaçao= input('[+],[-],[*],[/]')
+            n2=int(input('segundo numero'))
+
+            if operaçao =='+':
+                print(n1+n2)
+            elif operaçao =='-':
+                print(n1-n2)
+            elif operaçao =='*':
+                print(n1*n2)
+            elif operaçao =='/':
+                print(n1/n2)
+            else:
+                print('operaçao invalida')
+            
+            sair=False
+            while sair ==False : 
+                sair = input('[S] para sair [N] novamente')
+                
+                if sair=='s':
+                    rodando=False
+                    sair=True
+                elif sair =='n':
+                    
+                    sair=True
+                else:
+                    sair=False
+                 
+    except:
+            print('insira um numero valido')
+
+"""
+text ='vitovr'
+i=0
+qtd=0
+while i < len(text):
+    letra= text[i]
+    if letra ==' ':
+        i+=1
+        continue
+    contando_letra=text.count(letra)
+    if contando_letra >= qtd:
+        qtd=contando_letra
+        letra_max =[ letra, qtd]
+    i+=1
+print(letra_max)
